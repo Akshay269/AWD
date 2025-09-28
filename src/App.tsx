34daa69da1afbff54fe2f2fee63ff1ba7b36d7cf
Akshay269 <aksrahangdale@gmail.com>
2025-09-28 21:49:39 +0530

    video quality enchanced

--- src/App.tsx
+++ src/App.tsx
@@ -47,9 +47,9 @@ function App() {
           <main>
             {/* Hero Section loads immediately */}
             <Intro
-              desktopVideo="https://res.cloudinary.com/dsgbgr8or/video/upload/q_auto,f_auto,w_1920/v1758097779/website_avhwvn.webm"
-              mobileVideo="https://res.cloudinary.com/dsgbgr8or/video/upload/q_auto,f_auto,w_640/v1758132494/website-mobile_vih5nc.webm"
-              poster="https://res.cloudinary.com/dsgbgr8or/image/upload/q_auto,f_auto,w_1200/v1758273485/LOGO_uhfbf5.png"
+              desktopVideo="https://res.cloudinary.com/dsgbgr8or/video/upload/f_auto,q_auto:best,w_auto,dpr_auto/v1758097779/website_avhwvn.webm"
+              mobileVideo="https://res.cloudinary.com/dsgbgr8or/video/upload/f_auto,q_auto:best,w_auto,dpr_auto/v1758132494/website-mobile_vih5nc.webm"
+              poster="https://res.cloudinary.com/dsgbgr8or/image/upload/f_auto,q_auto:best,w_auto,dpr_auto/v1758273485/LOGO_uhfbf5.png"
             />
 
             {/* Lazy sections (load only when scrolled near) */}
